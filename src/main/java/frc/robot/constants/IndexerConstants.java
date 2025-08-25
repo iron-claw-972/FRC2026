0b605b71b21ef7b608a535019a97afa8f53b4693
eileha <eileenhan369@gmail.com>
2025-08-25 16:22:08 -0700

    rewrote indexer code
    did not include simulation stuff & logging stuff

---
+++ src/main/java/frc/robot/constants/IndexerConstants.java
@@ -0,0 +1,11 @@
+package frc.robot.constants;
+
+public class IndexerConstants {
+    public static final double speed = 1;
+
+    public static final double MOMENT_OF_INERTIA = 0.000326;
+    public static final double GEAR_RATIO = 1.0;
+    public  static final int MEASUREMENT_THRESHOLD = 15;// in milimeters
+
+
+}
