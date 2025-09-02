38615578962de93f3155c5be9e54a6dc9c821e54
eileha <eileenhan369@gmail.com>
2025-09-02 16:12:38 -0700

    rewrite arm
    wrote the methods without anything in them first.. will continue

--- src/main/java/frc/robot/subsystems/ArmBase.java
+++ src/main/java/frc/robot/subsystems/ArmBase.java
@@ -4,4 +4,25 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public abstract class ArmBase extends SubsystemBase {
     
+    public abstract void setSetpoint(double setpoint);
+
+    public abstract double getAngle();
+
+    public abstract boolean atSetpoint();
+
+    // periodic()?
+
+    public double getAppliedVoltage(){
+         
+    }
+
+    public void resetAbsolute(){
+
+    }
+
+    public boolean canMoveElevator(){
+
+    }
+
+
 }
