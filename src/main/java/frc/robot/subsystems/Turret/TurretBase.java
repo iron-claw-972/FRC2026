be97396628ec55258bcd0a90ffa7343a03e4db64
ethan863 <ethanmortensen20@gmail.com>
2025-09-05 16:16:19 -0700

    Update TurretBase.java
    Adding methods for our motor on the turret

--- src/main/java/frc/robot/subsystems/Turret/TurretBase.java
+++ src/main/java/frc/robot/subsystems/Turret/TurretBase.java
@@ -1,5 +1,13 @@
 package frc.robot.subsystems.Turret;
 
 public class TurretBase {
-    
+    public void SetMotor(TalonFX motor, double speed){
+        motor.set(speed);
+    }
+    public void StopMotor(TalonFX motor){
+        SetMotor(motor, 0);
+    }
+    public double GetPosition(TalonFX motor){
+        return motor.getPosition().getValue();
+    }
 }
