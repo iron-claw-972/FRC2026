965f85ebe86d93ec5c2d45389474a650595198e3
eileha <eileenhan369@gmail.com>
2025-09-10 16:56:00 -0700

    erm idk what im doing
    trying arm simulation

--- src/main/java/frc/robot/subsystems/ArmComp.java
+++ src/main/java/frc/robot/subsystems/ArmComp.java
@@ -1,17 +1,34 @@
+package frc.robot.subsystems;
+
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.vision.ReturnData;
 import frc.robot.constants.ArmConstants;
+import frc.robot.constants.Constants;
 import frc.robot.constants.IdConstants;
 
 public class ArmComp extends ArmBase {
  
+    private static final DCMotor TalonFX = null;
     private TalonFX motor = new TalonFX(IdConstants.ARM_MOTOR);
     private PIDController pid = new PIDController(0.1, 0, 0);
 
     private double setpoint = ArmConstants.START_ANGLE;
+
+    private SingleJointedArmSim armSim;
+
+    // sim? motor idk which one to use
+    DCMotor dcMotor;
+    // simulation Objects
+    Mechanism2d mechanism2d;
+    MechanismLigament2d ligament2d;
 
     // TODO: fix gear ratio
     double gearRatio = 27.5;
@@ -22,6 +39,16 @@ public class ArmComp extends ArmBase {
         // Ideally, the PID target value should also be zero
         // because if we were to call getAngle() right now, it would be zero rather than START_ANGLE.
         setSetpoint(ArmConstants.START_ANGLE);
+        // simulation Arm
+        armSim = new SingleJointedArmSim(dcMotor, gearRatio, 
+        0.1, 0.05, 
+        Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY, 
+        false, ArmConstants.START_ANGLE, null);
+        mechanism2d = new Mechanism2d(100, 100);
+        ligament2d = new MechanismLigament2d("Arm try", 25, ArmConstants.START_ANGLE);
+        mechanism2d.getRoot("pivot", 50, 50).append(ligament2d);
+        SmartDashboard.putData("Arm Display", mechanism2d);
+
     }
 
     @Override
@@ -33,13 +60,20 @@ public class ArmComp extends ArmBase {
         motor.set(power); 
     }
 
+    public void simulationPeriodic(){
+        double position = getAngle();
+        armSim.update(Constants.LOOP_TIME);
+        ligament2d.setAngle(position);
+        setSetpoint(pid.calculate(Units.degreesToRadians(position)));
+    }
+
     /**
      * Set the target position (angle) of the arm.
      * @param setpoint The target angle in degrees
      */
     @Override
     public void setSetpoint(double setpoint){
-        // Tell the PID object what the setpoint is
+        // Tell the PID object what thsete setpoint is
         // PID is using radians
         pid.setSetpoint(Units.degreesToRadians(setpoint * gearRatio));
 
