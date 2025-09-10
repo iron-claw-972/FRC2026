3b038d57284540cfc83154114bfd7c8c5077840d
ethan863 <ethanmortensen20@gmail.com>
2025-09-10 16:20:29 -0700

    Update TurretBase.java

--- src/main/java/frc/robot/subsystems/Turret/TurretBase.java
+++ src/main/java/frc/robot/subsystems/Turret/TurretBase.java
@@ -3,17 +3,18 @@ package frc.robot.subsystems.Turret;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.IdConstants;
 
 public class TurretBase extends SubsystemBase {
     double position;
-    private TalonFX turrotBaseMotor;
+    private TalonFX turretBaseMotor;
     TurretBase(double positon) {
-        this.turrotBaseMotor = new TalonFX(IdConstants.Base_ID, "rio");
+        this.turretBaseMotor = new TalonFX(IdConstants.Base_ID, "rio");
 
-        this.turrotBaseMotor.setNeutralMode(NeutralModeValue.Coast);
-        this.turrotBaseMotor.set(0);
+        this.turretBaseMotor.setNeutralMode(NeutralModeValue.Coast);
+        this.turretBaseMotor.set(0);
     }
     public void SetMotor(TalonFX motor, double speed){
         motor.set(speed);
@@ -21,7 +22,7 @@ public class TurretBase extends SubsystemBase {
     public void StopMotor(TalonFX motor){
         SetMotor(motor, 0);
     }
-    public double GetPosition(TalonFX motor){
+    public Angle GetPosition(TalonFX motor){
         return motor.getPosition().getValue();
     }
 }
