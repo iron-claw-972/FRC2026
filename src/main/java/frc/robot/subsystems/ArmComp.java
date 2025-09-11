769e0e1385681d2333ae8323a696f3fd5667c61a
eileha <eileenhan369@gmail.com>
2025-09-11 16:26:08 -0700

    updated the simluation stuff

--- src/main/java/frc/robot/subsystems/ArmComp.java
+++ src/main/java/frc/robot/subsystems/ArmComp.java
@@ -1,14 +1,21 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.sim.TalonFXSimState;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.simulation.BatterySim;
+import edu.wpi.first.wpilibj.simulation.EncoderSim;
+import edu.wpi.first.wpilibj.simulation.RoboRioSim;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.commands.vision.ReturnData;
 import frc.robot.constants.ArmConstants;
 import frc.robot.constants.Constants;
@@ -16,16 +23,14 @@ import frc.robot.constants.IdConstants;
 
 public class ArmComp extends ArmBase {
 
-    private static final DCMotor TalonFX = null;
+    private static final DCMotor simMotor = DCMotor.getKrakenX60(1);
     private TalonFX motor = new TalonFX(IdConstants.ARM_MOTOR);
     private PIDController pid = new PIDController(0.1, 0, 0);
-
-    private double setpoint = ArmConstants.START_ANGLE;
+    private TalonFXSimState encoderSim = motor.getSimState(); 
 
     private SingleJointedArmSim armSim;
 
     // sim? motor idk which one to use
-    DCMotor dcMotor;
     // simulation Objects
     Mechanism2d mechanism2d;
     MechanismLigament2d ligament2d;
@@ -41,15 +46,24 @@ public class ArmComp extends ArmBase {
         // START_ANGLE.
         setSetpoint(ArmConstants.START_ANGLE);
         // simulation Arm
-        armSim = new SingleJointedArmSim(dcMotor, gearRatio,
-                0.1, 0.05,
-                Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY,
-                false, ArmConstants.START_ANGLE, null);
+        armSim = new SingleJointedArmSim(
+            simMotor, 
+            gearRatio,
+            0.1, 
+            0.05,
+            Double.POSITIVE_INFINITY, 
+            Double.NEGATIVE_INFINITY,
+            false, 
+            Units.degreesToRadians(ArmConstants.START_ANGLE));
+
         mechanism2d = new Mechanism2d(100, 100);
-        ligament2d = new MechanismLigament2d("Arm try", 25, ArmConstants.START_ANGLE);
+        ligament2d = new MechanismLigament2d("Arm", 25, ArmConstants.START_ANGLE);
         mechanism2d.getRoot("pivot", 50, 50).append(ligament2d);
         SmartDashboard.putData("Arm Display", mechanism2d);
-
+        SmartDashboard.putData("Set 90 degrees", new InstantCommand(() -> setSetpoint(90)));
+        SmartDashboard.putData("Set 180 degrees", new InstantCommand(() -> setSetpoint(180)));
+        SmartDashboard.putData("Set 0 degrees", new InstantCommand(() -> setSetpoint(0)));
+        SmartDashboard.putData("Set 67 degrees", new InstantCommand(() -> setSetpoint(67)));
     }
 
     @Override
@@ -61,11 +75,18 @@ public class ArmComp extends ArmBase {
         motor.set(power);
     }
 
+    @Override
     public void simulationPeriodic() {
-        double position = getAngle();
+        // Get the drive to the motor (motor voltage)
+        armSim.setInput(motor.get() * 12); 
+        // Simulate the system for one time step 
         armSim.update(Constants.LOOP_TIME);
-        ligament2d.setAngle(position);
-        setSetpoint(pid.calculate(Units.degreesToRadians(position)));
+        double simAngle = armSim.getAngleRads(); 
+        double simRotations = Units.radiansToRotations(simAngle);
+        double motorRotations = simRotations * gearRatio; 
+        encoderSim.setRawRotorPosition(motorRotations);
+        ligament2d.setAngle(Units.radiansToDegrees(simAngle));
+       
     }
 
     /**
@@ -78,9 +99,7 @@ public class ArmComp extends ArmBase {
         // Tell the PID object what thsete setpoint is
         // PID is using radians
         pid.setSetpoint(Units.degreesToRadians(setpoint * gearRatio));
-
-        this.setpoint = setpoint;
-    }
+        }
 
     /** Gets the arm angle in degrees */
     @Override
@@ -92,7 +111,7 @@ public class ArmComp extends ArmBase {
 
     @Override
     public boolean atSetpoint() {
-        return (Math.abs(setpoint - getAngle()) < ArmConstants.TOLERANCE);
+        return (Math.abs(pid.getSetpoint() - getAngle()) < ArmConstants.TOLERANCE);
     }
 
     public double getAppliedVoltage() {
