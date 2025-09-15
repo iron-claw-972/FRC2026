04f9e9c8069aec1286eb937a3e63765c177ccad5
ethan863 <ethanmortensen20@gmail.com>
2025-09-15 14:43:37 -0700

    Turret Methods
    Added a lot of methods to our turret spinning base

--- src/main/java/frc/robot/constants/IdConstants.java
+++ src/main/java/frc/robot/constants/IdConstants.java
@@ -17,7 +17,7 @@ public class IdConstants {
     public static final int PIGEON = 13;
 
     // Ultimate Turret
-    public static final int Base_ID = 7;
+    public static final int BASE_MOTOR_ID = 67;
 
     // LEDs
     public static final int CANDLE_ID = 1;
--- src/main/java/frc/robot/subsystems/Turret/TurretBase.java
+++ src/main/java/frc/robot/subsystems/Turret/TurretBase.java
@@ -1,29 +1,72 @@
 package frc.robot.subsystems.Turret;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.NeutralModeValue;
+import java.util.random.RandomGenerator.LeapableGenerator;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.Unit;
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
 import frc.robot.constants.IdConstants;
 
 public class TurretBase extends SubsystemBase {
-    double position;
-    private TalonFX turretBaseMotor;
-    TurretBase(double positon) {
-        this.turretBaseMotor = new TalonFX(IdConstants.Base_ID, "rio");
-
-        this.turretBaseMotor.setNeutralMode(NeutralModeValue.Coast);
-        this.turretBaseMotor.set(0);
+    private TalonFX motor;
+    private final DigitalInput sensor = new DigitalInput(0);
+    private double position;
+    private double velocity;
+    private PIDController pid = new PIDController(0.02, 0, 0);
+    private boolean sensorTriggered;
+    private boolean motorCalibrated;
+    public TurretBase(){
+        motor = new TalonFX(IdConstants.BASE_MOTOR_ID);
+        position = 0;
+        velocity = 0;
+        pid.setTolerance(0.2);
+        sensorTriggered = false;
+        motorCalibrated = false;
     }
-    public void SetMotor(TalonFX motor, double speed){
-        motor.set(speed);
+    public double getPosition(){
+        return position;
     }
-    public void StopMotor(TalonFX motor){
-        SetMotor(motor, 0);
+    public void spinTo(double setPoint){
+        pid.reset();
+        pid.setSetpoint(setPoint);
     }
-    public Angle GetPosition(TalonFX motor){
-        return motor.getPosition().getValue();
+    public boolean atSetPoint(){
+        return pid.atSetpoint();
+    }
+    public double getVelocity(){
+        return velocity;
+    }
+    public boolean isSensorTriggered(){
+        return sensorTriggered;
+    }
+    public boolean isMotorCalibrated(){
+        return motorCalibrated;
+    }
+    public void calibrate(){
+        if(!sensorTriggered){
+            motor.set(0.02);
+        }
+        else{
+            motor.stopMotor();
+            motorCalibrated = true;
+        }
+    }
+    @Override
+    public void periodic(){
+        if(!motorCalibrated){
+            calibrate();
+        }
+        position = Units.rotationsToDegrees(motor.getPosition().getValueAsDouble());
+        velocity = motor.getVelocity().getValueAsDouble();
+        pid.calculate(Units.degreesToRadians(getPosition()));
+        sensorTriggered = sensor.get();
     }
 }
 
