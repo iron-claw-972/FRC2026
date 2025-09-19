3f93093978777ca38c4e4f4063551c627d705ee4
Wesley28w <wesleycwong@gmail.com>
2025-09-19 15:49:58 -0700

    bybgh gy7hb

--- src/main/java/frc/robot/constants/IdConstants.java
+++ src/main/java/frc/robot/constants/IdConstants.java
@@ -17,7 +17,11 @@ public class IdConstants {
     public static final int PIGEON = 13;
 
     // Ultimate Turret
+<<<<<<< Updated upstream
     public static final int BASE_MOTOR_ID = 67;
+=======
+    public static final int Base_Motor_ID = 67;
+>>>>>>> Stashed changes
 
     // LEDs
     public static final int CANDLE_ID = 1;
