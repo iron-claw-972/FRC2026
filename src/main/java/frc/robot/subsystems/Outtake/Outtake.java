1451d15e1587f235d420aa0e5920673248376a98
iefomit <timofei.stem@gmail.com>
2025-09-22 10:35:32 -0700

    Update Outtake.java
    changed motor type, modified color sensor, + methods

--- src/main/java/frc/robot/subsystems/Outtake/Outtake.java
+++ src/main/java/frc/robot/subsystems/Outtake/Outtake.java
@@ -1,29 +1,66 @@
 package frc.robot.subsystems.Outtake;
 
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.revrobotics.ColorSensorV3;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkFlex;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkFlexConfig;
 
-import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.hal.I2CJNI;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.IdConstants;
 
 public class Outtake extends SubsystemBase {
 
-    private SparkFlex motor = new SparkFlex(IdConstants.OUTTAKE_MOTOR, MotorType.kBrushless);
+    private TalonFX motor = new TalonFX(IdConstants.OUTTAKE_MOTOR);
     private double power;
 
     /** color sensor (measures coral based on proximity) - detects before rollers */
-    private final ColorSensorV3 colorSensor = new ColorSensorV3(null); // change to proper channel
-    /** beam break sensor - detects after rollers */
-    private DigitalInput digitalInputEjecting = new DigitalInput(0); // change to proper channel
+    private ColorSensorV3 colorSensor = new ColorSensorV3(I2C.Port.kOnboard);
+
 
     public Outtake() {
-        motor.configure(new SparkFlexConfig().inverted(true).idleMode(IdleMode.kBrake), ResetMode.kResetSafeParameters,
-        PersistMode.kNoPersistParameters);;
+        motor.getConfigurator().apply(new MotorOutputConfigs().withInverted(InvertedValue.CounterClockwise_Positive).withNeutralMode(NeutralModeValue.Brake));
+    }
+
+
+    public double getMotorSpeed() {
+        return power;
+    }
+    @Override
+    public void periodic() {
+        motor.set(power);
+    }
+
+    public void setMotor(double power) {
+        this.power = power;
+    }
+
+    public void outtake(){
+        setMotor(-0.3);
+    }
+
+    public int getProximity() {
+        int proximity = colorSensor.getProximity();
+        if (proximity > 0) {
+            return proximity;
+        } else {
+            I2CJNI.i2CClose(1);
+            colorSensor = new ColorSensorV3(I2C.Port.kOnboard);
+            return colorSensor.getProximity();
+        }
+    }
+
+    public void reverse(){
+        setMotor(0.3);
+    }
+
+    public boolean coralLoaded() {
+        return getProximity() > 2000;
+    }
+
+    public boolean coralEjecting() {
+        return coralLoaded();
     }
 }
