46fd47e53e86a8061776a6cf90a513f72b63400b
Wesley28w <wesleycwong@gmail.com>
2025-10-30 14:36:37 -0700

    Constants

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -2,14 +2,16 @@ package frc.robot.constants;
 
 public class IntakeConstants {
     //TODO: Update these constants
-    public static final double PIVOT_GEAR_RATIO = 1/(18.0*18.0*10.0/54.0/60.0/38.0);
+    public static final double PIVOT_GEAR_RATIO = 1/(18.0*18.0*10.0/54.0/60.0/38.0);  //38
 
-    public static final double MASS = 2.46; // kilograms
-    public static final double MOI = 0.0261057394; // kg*m^2
-    public static final double CENTER_OF_MASS_LENGTH = 0.138; // meters
+    public static final double MASS = 1.5753263; // kilograms: mass of arm specifically
+    public static final double CENTER_OF_MASS_LENGTH = 0.199608903192622; // meters
     public static final double LENGTH = CENTER_OF_MASS_LENGTH * 2; // meters
-    public static final double MAX_VELOCITY = 21; // rad/s
-    public static final double MAX_ACCELERATION = 120; // rad/s^2
 
-    public static final double START_ANGLE = 0;
+    public static final double MOI = MASS * LENGTH; // kg*m^2
+    public static final double MAX_VELOCITY = 2; // rad/s
+    public static final double MAX_ACCELERATION = 10; // rad/s^2
+
+    public static final double START_ANGLE = 101.7539148;
+    public static final double END_ANGLE = 153.7635904;
 }
