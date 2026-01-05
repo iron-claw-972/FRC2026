8d2742a25a183bd41807262a37b70fbbc1a53621
maxwtan <100314265+MaxwellTTan20@users.noreply.github.com>
2026-01-04 22:49:46 -0800

    Update PS5ControllerDriverConfig.java

--- src/main/java/frc/robot/controls/PS5ControllerDriverConfig.java
+++ src/main/java/frc/robot/controls/PS5ControllerDriverConfig.java
@@ -5,6 +5,7 @@ import java.util.function.BooleanSupplier;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -220,6 +221,7 @@ public class PS5ControllerDriverConfig extends BaseDriverConfig {
         double XDifference = tagpose.getX()-drivepose.getX();
         double angle = Math.atan(YDifference/XDifference);
         alignmentPose = new Pose2d(drivepose.getX(), drivepose.getY(), new Rotation2d(angle));
+        System.out.println("Alignment Angle: " + Units.radiansToDegrees(angle));
     }
 
   
