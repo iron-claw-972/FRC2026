256bba3b217784c29fa925cd97348cd7b9807e7b
ericoj100 <144740721+ericoj100@users.noreply.github.com>
2026-01-05 16:15:17 -0800

    cod

--- src/main/java/frc/robot/constants/swerve/SwerveConstants.java
+++ src/main/java/frc/robot/constants/swerve/SwerveConstants.java
@@ -1,13 +1,75 @@
 // package frc.robot.constants.swerve;
 
 // public enum SwerveConstants {
-//     one, two, three;
+//     WAFFLE_HOUSE(), 
+
+//     // GEAR RATIOS
 //     private final double driveGearRatio;
 //     private final double angleGearRatio; 
 
+//     // FRONT LEFT ID CONSTANTS
+//     private final double frontLeftDriveId;
+//     private final double frontLeftSteerId;
+//     private final double frontLeftEncoderId; 
+
+//     // FRONT RIGHT ID CONSTANTS
+//     private final double frontRightDriveId;
+//     private final double frontRightSteerId;
+//     private final double frontRightEncoderId; 
+
+//     // BACK LEFT ID CONSTANTS
+//     private final double backLeftDriveId; 
+//     private final double backLeftSteerId;
+//     private final double backLeftEncoderId; 
+
+//     // BACK RIGHT ID CONSTANTS
+//     private final double backRightDriveId; 
+//     private final double backRightSteerId;
+//     private final double backRightEncoderId; 
+
+//     // OFFSETS
+//     private final double frontLeftEncoderOffset;
+//     private final double frontRightEncoderOffset;
+//     private final double backLeftEncoderOffset;
+//     private final double backRightEncoderOffset; 
+
 //     SwerveConstants(
-
+//         double driveGearRatio,
+//         double angleGearRatio,
+//         double frontLeftDriveId,
+//         double frontLeftSteerId,
+//         double frontLeftEncoderId,
+//         double frontRightDriveId,
+//         double frontRightSteerId,
+//         double frontRightEncoderId,
+//         double backLeftDriveId,
+//         double backLeftSteerId,
+//         double backLeftEncoderId, 
+//         double backRightDriveId, 
+//         double backRightSteerId, 
+//         double backRightEncoderId,
+//         double frontLeftEncoderOffset,
+//         double frontRightEncoderOffset,
+//         double backLeftEncoderOffset,
+//         double backRightEncoderOffset
 //     ) {
-
+//         this.driveGearRatio = driveGearRatio; 
+//         this.angleGearRatio = angleGearRatio; 
+//         this.frontLeftDriveId = frontLeftDriveId; 
+//         this.frontLeftSteerId = frontLeftSteerId; 
+//         this.frontLeftEncoderId = frontLeftEncoderId; 
+//         this.frontRightDriveId = frontRightDriveId; 
+//         this.frontRightSteerId = frontRightSteerId; 
+//         this.frontRightEncoderId = frontRightEncoderId; 
+//         this.backLeftDriveId = backLeftDriveId; 
+//         this.backLeftSteerId = backLeftSteerId; 
+//         this.backLeftEncoderId = backLeftEncoderId; 
+//         this.backRightDriveId = backRightDriveId; 
+//         this.backRightSteerId = backRightSteerId; 
+//         this.backRightEncoderId = backRightEncoderId; 
+//         this.frontLeftEncoderOffset = frontLeftEncoderOffset; 
+//         this.frontRightEncoderOffset = frontRightEncoderOffset;
+//         this.backLeftEncoderOffset = backLeftEncoderOffset; 
+//         this.backRightEncoderOffset = backRightEncoderOffset; 
 //     }
 // }
