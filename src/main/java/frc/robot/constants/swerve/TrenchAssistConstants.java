325ddcff7f330d54917c9f9a915b77a12c715e32
moo <moogoesmeow123@gmail.com>
2026-01-15 13:57:11 -0800

    much good numbies

--- src/main/java/frc/robot/constants/swerve/TrenchAssistConstants.java
+++ src/main/java/frc/robot/constants/swerve/TrenchAssistConstants.java
@@ -12,7 +12,10 @@ public class TrenchAssistConstants {
     }; //8.07m
 
     public static final Rectangle2d[] ALIGN_ZONES = new Rectangle2d[]{
-        new Rectangle2d(new Translation2d(4.03, 1.28), new Translation2d(5.22, 1.58))
+        new Rectangle2d(new Translation2d(4.03 - .5, 0), new Translation2d(5.22 + .5, 3)),
+        new Rectangle2d(new Translation2d(4.03 - .5, 8.07), new Translation2d(5.22 + .5, 8.07 - 3)),
+        new Rectangle2d(new Translation2d(11.32 - .5, 0), new Translation2d(12.51 + .5, 3)),
+        new Rectangle2d(new Translation2d(11.32 - .5, 8.07), new Translation2d(12.51 + .5, 8.07 - 3)),
     };
 
 
