f0e4a58a657ab60bde9439385865f149c276e9fb
mixxlto <maxtan0626@gmail.com>
2026-01-18 15:20:35 -0800

    restored fieldconstants to store stuff like hub location

---
+++ src/main/java/frc/robot/constants/FieldConstants.java
@@ -0,0 +1,15 @@
+package frc.robot.constants;
+
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+
+public class FieldConstants {
+  /** Width of the field [meters] */
+  public static final double FIELD_LENGTH = Units.inchesToMeters(57*12 + 6+7.0/8.0);
+  /** Height of the field [meters] */
+  public static final double FIELD_WIDTH = Units.inchesToMeters(26*12 + 5);
+
+  /** Location of hub target */
+  public static final Translation3d HUB_TRANSLATION3D = new Translation3d(Units.inchesToMeters(156.8), 4.035, Units.inchesToMeters(72));
+
+}
