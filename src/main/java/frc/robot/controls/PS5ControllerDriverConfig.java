0d5a221f719e5c3d5f7e95d51390a23d1774720f
mixxlto <maxtan0626@gmail.com>
2026-01-18 20:02:07 -0800

    Update PS5ControllerDriverConfig.java

--- src/main/java/frc/robot/controls/PS5ControllerDriverConfig.java
+++ src/main/java/frc/robot/controls/PS5ControllerDriverConfig.java
@@ -68,7 +68,7 @@ public class PS5ControllerDriverConfig extends BaseDriverConfig {
         Translation2d drivepose = getDrivetrain().getPose().getTranslation();
         // uses tag #??
         int tagNumber = 17;
-        Translation2d tagpose = FieldConstants.APRIL_TAGS.get(tagNumber - 1).pose.toPose2d().getTranslation();
+        Translation2d tagpose = FieldConstants.field.getTagPose(tagNumber).get().toPose2d().getTranslation();
         double YDifference = tagpose.getY() - drivepose.getY();
         double XDifference = tagpose.getX() - drivepose.getX();
         double angle = Math.atan(YDifference/XDifference);
