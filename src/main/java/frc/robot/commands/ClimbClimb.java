f2c08a0b61acf0ec06a839bc0dd1735ad5611a98
moo <moogoesmeow123@gmail.com>
2026-01-20 17:18:04 -0800

    climb command
    i guess its a command, doesn't really do much

---
+++ src/main/java/frc/robot/commands/ClimbClimb.java
@@ -0,0 +1,36 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.climb.Climb;
+
+public class ClimbClimb extends Command {
+    Climb climb;
+
+    public final double CLIMB_SETPOINT = 1.0; //TODO add proper value
+
+    public ClimbClimb(Climb climb){
+        this.climb = climb;
+        addRequirements(climb);
+    }
+
+    @Override
+    public void initialize() {
+        climb.setSetpoint(CLIMB_SETPOINT);
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return climb.atSetpoint();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        
+    }
+
+}
