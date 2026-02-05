0a3c81fcf774605f0227b444c49c4a68ff2cad2b
eileha <eileenhan369@gmail.com>
2026-02-05 15:02:29 -0800

    updated

--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -5,6 +5,7 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.MotorArrangementValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.revrobotics.spark.config.SparkMaxConfig;
@@ -19,7 +20,6 @@ public class Intake extends SubsystemBase {
     final int rightID = 1;
     final int leftID = 2;
     final int rollerID = 3;
-    final String canBus = "CANivoreSub";
 
     private TalonFX rollerMotor; 
     private TalonFX rightMotor; //leader
@@ -31,27 +31,42 @@ public class Intake extends SubsystemBase {
 
     public Intake() {
        // set actual IDs
-        rightMotor = new TalonFX(rightID, canBus);
-        leftMotor = new TalonFX(leftID, canBus);
-        rollerMotor = new TalonFX(rollerID, canBus); 
-        TalonFXConfiguration config = new TalonFXConfiguration();
-        var slot0Configs = config.Slot0;
+        rightMotor = new TalonFX(rightID);
+        leftMotor = new TalonFX(leftID);
+        rollerMotor = new TalonFX(rollerID); 
+
+        // right motor configs
+        TalonFXConfiguration rightConfig = new TalonFXConfiguration();
+        var slot0RightConfigs = rightConfig.Slot0;
         //find values later
         //friction, maybe?
-        slot0Configs.kP = 0;
-        slot0Configs.kI = 0;
-        slot0Configs.kD = 0;
-        slot0Configs.kV = 0;
-        slot0Configs.kA = 0;
+        slot0RightConfigs.kP = 0;
+        slot0RightConfigs.kI = 0;
+        slot0RightConfigs.kD = 0;
+        slot0RightConfigs.kV = 0;
+        slot0RightConfigs.kA = 0;
+
+        // left motor configs
+        TalonFXConfiguration leftConfig = new TalonFXConfiguration();
+        var slot0LeftConfigs = leftConfig.Slot0;
+
+        slot0LeftConfigs.kP = 0;
+        slot0LeftConfigs.kI = 0;
+        slot0LeftConfigs.kD = 0;
+        slot0LeftConfigs.kV = 0;
+        slot0LeftConfigs.kA = 0;
+
 
         rightMotor.getConfigurator().apply(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake));
-        rightMotor.getConfigurator().apply(config);
+        rightMotor.getConfigurator().apply(rightConfig);
 
-        leftMotor.getConfigurator().apply(config);
         leftMotor.getConfigurator().apply(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake));
+        leftMotor.getConfigurator().apply(leftConfig);
 
-        Follower follower = new Follower(rightMotor.getDeviceID(), true);
-        leftMotor.setControl(new Follower(rightMotor.getDeviceID(), true));
+
+        //Follower follower = new Follower(rightMotor.getDeviceID(), true);
+        leftMotor.setControl(new Follower(rightMotor.getDeviceID(), MotorAlignmentValue.Opposed));
+        
 
         SmartDashboard.putData("Extend Intake", new InstantCommand(() -> extend()));
         SmartDashboard.putData("Retract Intake", new InstantCommand(() -> retract()));
