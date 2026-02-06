e435ba046ad89befb11af1d09ab7d83aeebe9517
maxwtan <100314265+MaxwellTTan20@users.noreply.github.com>
2026-02-05 17:04:00 -0800

    a

--- src/main/java/frc/robot/subsystems/turret/TurretConstants.java
+++ src/main/java/frc/robot/subsystems/turret/TurretConstants.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems.turret;
 
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 
 public class TurretConstants {
@@ -15,5 +14,11 @@ public class TurretConstants {
 
     public static double ROTATIONAL_VELOCITY_CONSTANT = 0.2;
 
-    public static Translation2d DISTANCE_FROM_ROBOT_CENTER = new Translation2d(0, 0);
+    public static double TURRET_GEAR_RATIO = 140; // the turret teeth count
+    public static double LEFT_ENCODER_RATIO = 70/11; // read right description
+    public static double RIGHT_ENCODER_RATIO = 28/3; // The amount of times this encoder turns for every time the turret turns
+    public static double ENCODER_COUNT_TOTAL = 8192; // how many intervals it can have, like clicks on a clock chat gpt explained to me
+
+    public static double LEFT_ENCODER_OFFSET = 0; // degrees
+    public static double RIGHT_ENCODER_OFFSET = 0; // degrees
 }
