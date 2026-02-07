c370c87b1a1163cce98fea792b418e237f9cda59
Ethan Mortensen <ethanmortensen20@gmail.com>
2026-02-07 10:13:33 -0800

    continuing fixing stuff
    my code was in the wrong branch by accident

---
+++ src/main/java/frc/robot/constants/Climb/ClimbConstants.java
@@ -0,0 +1,13 @@
+package frc.robot.constants.Climb;
+
+public class ClimbConstants {
+    // CHANGE LATER
+
+    public final static double CLIMB_GEAR_RATIO = 1 / 1;
+    public final static double MAX_VELOCITY = 0.1;
+    public final static double MAX_ACCELERATION = 1;
+    public final static double MIN_HEIGHT = 0;
+    public final static double MAX_HEIGHT = 1;
+    public final static double RADIUS = 1;
+    public final static double CLIMB_HEIGHT = 1;
+}
