2bcff1cdc5a5225050ff85b1a98c52a41a2eb62c
maxwtan <100314265+MaxwellTTan20@users.noreply.github.com>
2026-02-07 10:56:44 -0800

    Update TurretConstants.java

--- src/main/java/frc/robot/subsystems/turret/TurretConstants.java
+++ src/main/java/frc/robot/subsystems/turret/TurretConstants.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.turret;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 
 public class TurretConstants {
@@ -16,4 +17,6 @@ public class TurretConstants {
     public static double ROTATIONAL_VELOCITY_CONSTANT = 0.2;
 
     public static Translation2d DISTANCE_FROM_ROBOT_CENTER = new Translation2d(0, 0);
+
+    Translation3d TURRET_TO_CENTER_OF_ROBOT = new Translation3d(x, y, 0.32)
 }
