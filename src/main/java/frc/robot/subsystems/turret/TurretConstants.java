48cf42916e1075e52cd7219d7a1c96512f35cbb4
mixxlto <maxtan0626@gmail.com>
2026-02-09 15:09:09 -0800

    Update TurretConstants.java

--- src/main/java/frc/robot/subsystems/turret/TurretConstants.java
+++ src/main/java/frc/robot/subsystems/turret/TurretConstants.java
@@ -1,8 +1,7 @@
 package frc.robot.subsystems.turret;
 
-import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.units.measure.Angle;
 
 public class TurretConstants {
     public static double MAX_ANGLE = 180;
@@ -12,10 +11,11 @@ public class TurretConstants {
     public static double MAX_ACCELERATION = 10000000; // m/s^2
 
     // TODO: FIND THE TURRET WIDTH
-    public static double TURRET_WIDTH = Units.feetToMeters(1.0);
+    public static double TURRET_WIDTH = Units.inchesToMeters(6.4);
     public static double TURRET_RADIUS = TURRET_WIDTH / 2;
 
-    public static double TURRET_GEAR_RATIO = 140.0; // the turret teeth count
+    public static double TURRET_TEETH_COUNT = 140.0; // the turret teeth count
+    public static double TURRET_GEAR_RATIO = 36.81818182;
     public static double LEFT_ENCODER_RATIO = 70.0/11.0; // read right description
     public static double RIGHT_ENCODER_RATIO = 28.0/3.0; // The amount of times this encoder turns for every time the turret turns
     public static double ENCODER_COUNT_TOTAL = 8192.0; // how many intervals it can have, like clicks on a clock chat gpt explained to me
@@ -23,6 +23,6 @@ public class TurretConstants {
     public static double LEFT_ENCODER_OFFSET = 0; // degrees
     public static double RIGHT_ENCODER_OFFSET = 0; // degrees
 
-    public static Translation2d DISTANCE_FROM_ROBOT_CENTER = new Translation2d(0,0);
+    public static Translation3d DISTANCE_FROM_ROBOT_CENTER = new Translation3d(0,0, Units.inchesToMeters(22.172)); //meters
 
 }
