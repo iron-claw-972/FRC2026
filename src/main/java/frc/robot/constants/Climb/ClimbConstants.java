2553e96b08a3c66c472402122bd98178a17b4957
iefomit <timofei.stem@gmail.com>
2026-02-17 13:04:37 -0800

    delete unused constants

--- src/main/java/frc/robot/constants/Climb/ClimbConstants.java
+++ src/main/java/frc/robot/constants/Climb/ClimbConstants.java
@@ -5,10 +5,7 @@ public class ClimbConstants {
     // CHANGE LATER
     // gear ratio for converting motor rotations to linear distance
     public final static double CLIMB_GEAR_RATIO = 1.0 / 45.0;
-    public final static double MAX_VELOCITY = 1;
-    public final static double MAX_ACCELERATION = 0.3;
     public final static double RADIUS = 0.3;
-    public final static double CLIMB_HEIGHT = 4;
     public final static double OFFSET = 100.0;
     public final static double CLIMB_OFFSET = 80.0;
 
@@ -17,7 +14,6 @@ public class ClimbConstants {
     // NORMAL: Moderate current for PID-controlled movement
     // CLIMB: High current for full-power climbing
     public final static double CALIBRATION_CURRENT = 7.0;
-    public final static double NORMAL_CURRENT = 5.0;
     public final static double CLIMB_CURRENT = 42.0;
 
     // PID Constants
@@ -27,12 +23,10 @@ public class ClimbConstants {
     public final static double PID_TOLERANCE = 0.2;
 
     // Motor Limits
-    public final static double DEFAULT_CURRENT_LIMIT = NORMAL_CURRENT;
     public final static double MIN_POWER = -0.2;
     public final static double MAX_POWER = 0.2;
     public final static double CALIBRATION_POWER = 0.15;
 
     // Calibration
     public final static int CALIBRATION_COUNTER_LIMIT = 250;
-    public final static double CALIBRATION_POSITION_OFFSET = 1.0;
 }
