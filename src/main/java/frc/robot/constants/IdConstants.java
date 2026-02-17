cf1ed81962409bf20189089d9027171e4821c5bb
iefomit <timofei.stem@gmail.com>
2026-02-16 16:46:40 -0800

    updated id/amps

--- src/main/java/frc/robot/constants/IdConstants.java
+++ src/main/java/frc/robot/constants/IdConstants.java
@@ -35,7 +35,7 @@ public class IdConstants {
     public static final int HOOD_ID = 11;
 
     // Spindexer
-    public static final int SPINDEXER_ID = 12;
+    public static final int SPINDEXER_ID = 16;
 
     // Intake
     public static final int INTAKE_BASE_LEFT_ID = 13;
