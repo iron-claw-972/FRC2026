deb51e53cfc5201e64837488dd2bf6e91023925a
maxwtan <100314265+MaxwellTTan20@users.noreply.github.com>
2026-02-20 13:13:53 -0800

    Update SpindexerConstants.java

--- src/main/java/frc/robot/subsystems/spindexer/SpindexerConstants.java
+++ src/main/java/frc/robot/subsystems/spindexer/SpindexerConstants.java
@@ -2,4 +2,8 @@ package frc.robot.subsystems.spindexer;
 
 public class SpindexerConstants {
     public static final double spindexerVelocityWithBall = 6.0; // rps (for counting balls)
+    public static final double currentLimit = 40; //A
+    public static final double spindexerMaxPower = 0.5; 
+    public static final double CURRENT_SPIKE_LIMIT = 80;
+    public static final double CURRENT_TIME_LIMIT = 1.0; //s
 }
