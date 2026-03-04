4af537bf83cd85dc557ace25c48cb19724e1a41d
Wesley28w <wesleycwong@gmail.com>
2026-03-04 06:21:20 -0800

    rebdz

--- src/main/java/frc/robot/subsystems/turret/TurretConstants.java
+++ src/main/java/frc/robot/subsystems/turret/TurretConstants.java
@@ -40,23 +40,3 @@ public class TurretConstants {
     public static final double CALIBRATION_CURRENT_THRESHOLD = 9.0; // A
 
 }
-/*
-turret is 140
-left encoder is 15
-right encoder is 22
-
-turret cannot go beyond 15 * 22 teeth (330) (2.3 rotations)
-2.3 * 360 = 828 deg (range of values we can have in degrees)
-
-picking 532 deg
-
-e_1_val = 532 * (140/15) % 360 = 285.33_
-e_2_val = 532 * (140/22) % 360 = 145.45_45
-
-(n + (E/360))Gr = A
-n = number of possible countable rot
-array only needs to be 0 - less than the teeth count of the other gear.
-
-
-
-*/
