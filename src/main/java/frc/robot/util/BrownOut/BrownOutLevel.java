b81416fc449685505a8ea3dce682944f6766dbed
Wesley28w <wesleycwong@gmail.com>
2026-03-19 16:47:54 -0700

    i was interuppted

---
+++ src/main/java/frc/robot/util/BrownOut/BrownOutLevel.java
@@ -0,0 +1,15 @@
+package frc.robot.util.BrownOut;
+
+public class BrownOutLevel {
+    // in percentage values (0.0-1.0)
+    double shooterCurrent;
+    double hoodCurrent;
+    double spindexerCurrent;
+    double intakeCurrent;
+    double drivetrainCurrent;
+
+    public BrownOutLevel() {
+        
+    }
+}
+
