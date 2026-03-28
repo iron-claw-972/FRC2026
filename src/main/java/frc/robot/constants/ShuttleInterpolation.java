370fca084ad4f3b6dc7a889df796263fd1e02dda
iefomit <timofei.stem@gmail.com>
2026-03-28 09:39:43 -0700

    fix shuttle

--- src/main/java/frc/robot/constants/ShuttleInterpolation.java
+++ src/main/java/frc/robot/constants/ShuttleInterpolation.java
@@ -1,18 +1,22 @@
+package frc.robot.constants;
 
-// i'm going to get so many lines of code from this :) - Wesley
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+
 public class ShuttleInterpolation {
-    public static final IntpolationDoubleTreeMap hoodAngleMap = new InterpolationDoubleTreeMap();
-    public static final IntpolationDoubleTreeMap shooterVelocityMap = new InterpolationDoubleTreeMap();
+    public static final InterpolatingDoubleTreeMap newHoodMap = new InterpolatingDoubleTreeMap();
+    public static final InterpolatingDoubleTreeMap shooterVelocityMap = new InterpolatingDoubleTreeMap();
     /*
-    guide to tuning:
-    modify the left values in the parenthesis to change the distance of the shot.
-    setup the robot at that distance and shoot. Then modifty the right value to be more or less until it hits target perfectly.
-    Repeat
-    OR
-    Run robot, estimate distance, and tweak values when nobodies looking until it works.
-    */
+     * guide to tuning:
+     * modify the left values in the parenthesis to change the distance of the shot.
+     * setup the robot at that distance and shoot. Then modifty the right value to
+     * be more or less until it hits target perfectly.
+     * Repeat
+     * OR
+     * Run robot, estimate distance, and tweak values when nobodies looking until it
+     * works.
+     */
     static {
-        // we can be less aggressive: y = 0.65 * (1.34959x + 9.79618) 
+        // we can be less aggressive: y = 0.65 * (1.34959x + 9.79618)
         // will likely be this that requires tuning.
         shooterVelocityMap.put(0.0, 6.2075);
         shooterVelocityMap.put(1.00, 7.475);
@@ -24,7 +28,7 @@ public class ShuttleInterpolation {
         shooterVelocityMap.put(5.79, 11.7);
         shooterVelocityMap.put(25.0, 28.236);
 
-        // always shoot at low angle to ground. 
+        // always shoot at low angle to ground.
         newHoodMap.put(0.0, 80.0);
         newHoodMap.put(1.00, 80.0);
         newHoodMap.put(1.49, 80.0);
