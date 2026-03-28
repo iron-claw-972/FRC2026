b04d5de6bfd724b3f4594d3decf023efefefe5d2
WesleyWong-972 <wesleycwong@gmail.com>
2026-03-28 05:39:36 -0700

    new file (Commit farming)

---
+++ src/main/java/frc/robot/constants/ShuttleInterpolation.java
@@ -0,0 +1,11 @@
+
+// i'm going to get so many lines of code from this :) - Wesley
+public class ShuttleInterpolation {
+    public static final IntpolationDoubleTreeMap timeOfFlightMap = new InterpolationDoubleTreeMap();
+    public static final IntpolationDoubleTreeMap timeOfFlightMap = new InterpolationDoubleTreeMap();
+
+    static {
+        
+    }
+
+}
