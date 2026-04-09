96afea5408fcf932ec9c47084b4e321d1424a926
iefomit <timofei.stem@gmail.com>
2026-04-09 13:34:14 -0700

    somewhat working just spindexer already maxing out

--- src/main/java/frc/robot/controls/BaseDriverConfig.java
+++ src/main/java/frc/robot/controls/BaseDriverConfig.java
@@ -13,7 +13,7 @@ import frc.robot.util.MathUtils;
  */
 public abstract class BaseDriverConfig {
 
-    private final Drivetrain drive;
+    public final Drivetrain drive;
 
     private double previousHeading = 0;
 
