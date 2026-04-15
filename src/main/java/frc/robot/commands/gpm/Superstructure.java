17b04c801b92da57f25814df3415c39ad6469f09
Wesley28w <wesleycwong@gmail.com>
2026-04-15 08:12:21 -0700

    some fixes

--- src/main/java/frc/robot/commands/gpm/Superstructure.java
+++ src/main/java/frc/robot/commands/gpm/Superstructure.java
@@ -218,7 +218,7 @@ public class Superstructure extends Command {
         ChassisSpeeds robotRelVel = drivetrain.getChassisSpeeds();
 
         // Add a phase delay extrapolation component for latency delay
-        drivepose.exp(
+        drivepose = drivepose.exp(
             new Twist2d(
                 robotRelVel.vxMetersPerSecond * phaseDelay,
                 robotRelVel.vyMetersPerSecond * phaseDelay,
