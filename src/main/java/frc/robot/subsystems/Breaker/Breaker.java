a0c4179e26d9eb6c40fcd54967916264a51a03f4
Wesley28w <wesleycwong@gmail.com>
2026-04-17 16:54:06 -0700

    ok

--- src/main/java/frc/robot/subsystems/Breaker/Breaker.java
+++ src/main/java/frc/robot/subsystems/Breaker/Breaker.java
@@ -1,14 +1,46 @@
 package frc.robot.subsystems.Breaker;
 
+import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Breaker extends SubsystemBase {
+    public PowerDistribution pDis = new PowerDistribution();
     public Breaker() {
 
     }
+    
 
     @Override
     public void periodic() {
         
     }
+
+    // currently ignores other stuff
+    private double getAllCurrentsFromPowerDistribution() {
+        double[] currents = pDis.getAllCurrents();
+        double totalCurrent = 0;
+        for (int i=0; i<currents.length; i++) {
+            totalCurrent += currents[0];
+        }
+        return totalCurrent;
+    }
+
+    // // currently uses stator currents
+    // // IMPORTANT: technically we should be uses supply current but since all of our subsystems use stator...
+    // private double getAllCurrentsFromSubsystems() {
+    //     double currentWithoutDrivetrain = shooter.getLeftStatorCurrent() +
+    //         shooter.getRightStatorCurrent() +
+    //         intake.getLeftStatorCurrent() +
+    //         intake.getRightStatorCurrent() +
+    //         intake.getRollerStatorCurrent() +
+    //         hood.getStatorCurrent() +
+    //         turret.getStatorCurrent() +
+    //         spindexer.getStatorCurrent();
+    //     double drivetrainModuleCurrent = 0;
+    //     for (double current : drivetrain.getStatorCurrents()) {
+    //         drivetrainModuleCurrent += current;
+    //     }
+
+    //     return currentWithoutDrivetrain + drivetrainModuleCurrent;
+    // }
 }
