4ec9e715b5052d000489f0540e8efe52f2cf05f7
Wesley28w <wesleycwong@gmail.com>
2026-04-17 16:47:00 -0700

    save

---
+++ src/main/java/frc/robot/subsystems/Breaker/Breaker.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems.Breaker;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Breaker extends SubsystemBase {
+    public Breaker() {
+
+    }
+
+    @Override
+    public void periodic() {
+        
+    }
+}
