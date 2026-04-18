abaea836b6b8be475f0458c7e84a11e745bd1a55
WesleyWong-972 <wesleycwong@gmail.com>
2026-04-18 14:39:58 -0700

    dynamic version saving

---
+++ src/main/java/frc/robot/commands/gpm/DynamicRunSpindexer.java
@@ -0,0 +1,111 @@
+package frc.robot.commands.gpm;
+
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ScheduleCommand;
+import frc.robot.constants.Constants;
+import frc.robot.constants.IntakeConstants;
+import frc.robot.subsystems.Intake.Intake;
+import frc.robot.subsystems.hood.Hood;
+import frc.robot.subsystems.spindexer.Spindexer;
+import frc.robot.subsystems.spindexer.SpindexerConstants;
+import frc.robot.subsystems.turret.Turret;
+
+public class DynamicRunSpindexer extends Command {
+    private Spindexer spindexer;
+    private Turret turret;
+    private Hood hood;
+    private Intake intake;
+
+    private Debouncer jamDebouncer = new Debouncer(SpindexerConstants.JAM_DEBOUNCE_TIME, DebounceType.kRising); // if there is jam I would think this is 0 -> 1
+
+    private boolean reversing = false;
+    private boolean wasHoodForcedDown = false;
+
+    private Timer reverseTimer = new Timer();
+
+    private double storedIntakeSpeed = 0.0;
+    private boolean reverseIntakeAntiJamEnabled = true;
+    
+    public DynamicRunSpindexer(Spindexer spindexer, Turret turret, Hood hood, Intake intake) {
+        this.spindexer = spindexer;
+        this.turret = turret;
+        this.hood = hood;
+        this.intake = intake;
+
+        addRequirements(spindexer);
+    }
+
+        // public RunSpindexer(Spindexer spindexer) {
+        // this.spindexer = spindexer;
+        // addRequirements(spindexer);
+    // }
+
+    @Override
+    public void initialize() {
+        wasHoodForcedDown = hood.getHoodForcedDown();
+    }
+
+    @Override
+    public void execute() {
+        boolean hoodForcedDown = hood.getHoodForcedDown();
+        
+        if (wasHoodForcedDown && !hoodForcedDown) {
+            spindexer.maxSpindexer();
+        }
+        wasHoodForcedDown = hoodForcedDown;
+        
+        if (!turret.atSetpoint() || hoodForcedDown) {
+            spindexer.stopSpindexer();
+            reversing = false;
+            return; // this is so the balls don't fly out when unaligned
+        }
+        boolean jammed = spindexer.getStatorCurrent() > SpindexerConstants.JAM_CURRENT_THRESHOLD;
+        if (jamDebouncer.calculate(jammed)) {
+            reversing = true;
+            reverseTimer.reset();
+            reverseTimer.start();
+            if (reverseIntakeAntiJamEnabled && DriverStation.isAutonomous()) {
+                storedIntakeSpeed = intake.getSpeed();
+            }
+        }
+        if (!reversing) {
+            spindexer.maxSpindexer();
+        } else {
+            spindexer.reverseSpindexer();
+
+            if (reverseIntakeAntiJamEnabled && DriverStation.isAutonomous()) {
+                if (intake.getPosition() > IntakeConstants.INTERMEDIATE_EXTENSION + 1.0) {
+                    intake.spinReverse();
+                } else {
+                    intake.extend();
+                }
+            }
+
+            if (reverseTimer.hasElapsed(SpindexerConstants.REVERSE_DEBOUNCE_TIME)) {
+                reversing = false;
+                if (reverseIntakeAntiJamEnabled && DriverStation.isAutonomous()) {
+                    intake.spin(storedIntakeSpeed);
+                }
+            }
+        }
+        if (!Constants.DISABLE_SMART_DASHBOARD) {
+            SmartDashboard.putBoolean("Spindexer Jamming", reversing);
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        spindexer.stopSpindexer();
+        reversing = false;
+    }
+
+    // @Override
+    // public boolean isFinished() {
+    //     return false;  // never ends on its own
+    // }
+}
