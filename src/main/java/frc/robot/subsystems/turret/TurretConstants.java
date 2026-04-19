14ba8fe4f855de0ae5e752d55dde825f89467636
WesleyWong-972 <wesleycwong@gmail.com>
2026-04-19 10:04:30 -0700

    Tuned Turret (only kP and ffkV)

--- src/main/java/frc/robot/subsystems/turret/TurretConstants.java
+++ src/main/java/frc/robot/subsystems/turret/TurretConstants.java
@@ -24,7 +24,8 @@ public class TurretConstants {
 
 	public static final double EXTRAPOLATION_TIME_CONSTANT = 0.06;
 
-	public static final double FEEDFORWARD_KV = 0.06;
+	public static final double FEEDFORWARD_KV = 0.02
+    ;
 
     public static final double NORMAL_CURRENT_LIMIT = 40.0; // A
     public static final double CALIBRATION_CURRENT_LIMIT = 10.0; // A
