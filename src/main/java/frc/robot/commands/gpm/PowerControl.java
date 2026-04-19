5960847456c021c42d405f8492045cd2650198ce
Wesley28w <wesleycwong@gmail.com>
2026-04-18 18:49:56 -0700

    todo, make the command

---
+++ src/main/java/frc/robot/commands/gpm/PowerControl.java
@@ -0,0 +1,8 @@
+package frc.robot.commands.gpm;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class PowerControl extends Command {
+    // TODO
+    // should be the thing that adjusts current for each subsystem
+}
