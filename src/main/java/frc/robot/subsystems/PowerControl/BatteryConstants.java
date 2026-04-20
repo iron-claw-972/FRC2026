40119f141960cf3b97f84cd69d4ba7f6c4f66815
iefomit <timofei.stem@gmail.com>
2026-04-20 07:47:30 -0700

    quick format

--- src/main/java/frc/robot/subsystems/PowerControl/BatteryConstants.java
+++ src/main/java/frc/robot/subsystems/PowerControl/BatteryConstants.java
@@ -1,5 +1,5 @@
 package frc.robot.subsystems.PowerControl;
 
 public class BatteryConstants {
-    public static final double MAX_STARTING_VOLTS = 12.5; // V 
+    public static final double MAX_STARTING_VOLTS = 12.5; // V
 }
